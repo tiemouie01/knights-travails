fbbd5ecece302f29503600be43c7161554f14211
Timothy Miamba <timal0361@gmail.com>
2024-01-05 08:18:02 +0200

    Add knightMoves module

--- knightMoves.js
+++ knightMoves.js
@@ -2,27 +2,59 @@
 import Board from "./board.js";
 import Square from "./square.js";
 
+const getShortest = function getShortestPath(moves, endSquare) {
+  // Find the shortest array from all the moves arrays.
+  let lowestIndex = 0;
+
+  for (let i = 0; i < moves.length - 1; i += 1) {
+    if (moves[i][-1] === endSquare)
+      lowestIndex = i;
+  }
+
+  return moves[lowestIndex];
+};
+
 const formatSquares = function changeToSquareObjects(start, end) {
-  if (!start.x) return [Square(...start), Square(...end)];
+  if (start.x === undefined) return [Square(...start), Square(...end)];
   return [start, end];
 };
 
+const findMoves = function findIndexesOfMovesToEnd(
+  startSquare,
+  endSquare,
+  visitedSquares,
+) {
+  // Base case.
+  if (startSquare === endSquare) {
+    return [endSquare];
+  }
+
+  const visited = visitedSquares;
+  visited[startSquare] = true;
+
+  const adjacencyList = Board().getAdjacencyList()[startSquare];
+  const moves = [];
+  
+  for (let i = 0; i < adjacencyList.length; i += 1) {
+    const nextSquare = adjacencyList[i];
+    if (!visited[nextSquare]) {
+      const move = [startSquare];
+      moves.push(move.concat(findMoves(nextSquare, endSquare, visited)));
+    }
+  }
+
+  return getShortest(moves, endSquare);
+};
+
 export default function knightMoves(start, end) {
-    // Create the chess board and change arguments to Square objects.
-    const chessBoard = Board();
-    const [startSquare, endSquare] = formatSquares(start, end);
+  const board = Board();
+  const visited = new Array(64).fill(false);
+  const [startSquare, endSquare] = formatSquares(start, end);
 
-    console.log(startSquare, endSquare);
+  const startIndex = board.findIndex(board.getBoard(), startSquare);
+  const endIndex = board.findIndex(board.getBoard(), endSquare);
 
-    // Base case.
-    // if (chessBoard.equalTo(startSquare, endSquare)) return [startSquare];
-
-    // const squares = chessBoard.getBoard();
-    // const adjacencyList = chessBoard.getAdjacencyList();
-    // const moves = [];
-
-    // adjacencyList.forEach((index) => {
-    //     const move = [startSquare];
-    //     const nextSquare = 
-    // })
+  const moves = findMoves(startIndex, endIndex, visited);
+  
+  return moves;
 }
--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 // eslint-disable-next-line import/extensions
-import Board from "./board.js";
 import knightMoves from "./knightMoves.js";
 
-// const board = Board();
-
-// console.table(board.getBoard());
-// console.table(board.getAdjacencyList());
-knightMoves([0,0], [3,3]);
+console.table(knightMoves([0,0], [3,3]));
